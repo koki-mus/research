3d9cae97eec2194c63a8f4e202a37fdb45104887
koki-mus <lkxl1239@yahoo.co.jp>
2022-12-19 15:55:45 +0900

    viewer and writer complete

--- src/writer.py
+++ src/writer.py
@@ -4,11 +4,11 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 cors = CORS(app, supports_credentials=True)
-
+predoc = " "
 @app.route("/postdata", methods=["POST"])
 def writedata():
-    response = jsonify({"token":"dummy"})
     req = request.json[0]
+    response = {"status":"success", "message":""}
     try:
         with open(req["filepath"], "a") as f:
             # f.write(str(request.json[0]))
@@ -18,18 +18,41 @@ def writedata():
             #  'locnumx2': 0, 'locnumy2': 0,
             #  'rangenumx': 0, 'rangenumy': 0}
             doc = str(request.json[0])
-            f.write(doc)
-        return "Succes"
+            # snappath,dataset,para,job,center[x,y],xrange[a,z], yrange[a,z]
+            dataset, para, job = req['snappath'][-12:-10], req['snappath'][-9:-7], req['snappath'][-6:-4] 
+            centerx = req["locnumx"]
+            centery = req["locnumy"]
+            xlow = req["locnumx"]
+            xup = int(req["locnumx"]) + int(req["rangenumx"])
+            ylow = req["locnumy"]
+            yup = int(req["locnumy"]) + int(req["rangenumy"])
+            doc = f"{req['snappath']},{dataset},{para},{job},{centerx},{centery},{xlow},{xup},{ylow},{yup}\n"
+            global predoc
+            if int(req["rangenumx"]) == 0 or int(req["rangenumy"]) == 0:
+                # return "error:range is 0"
+                response["message"] = "error:range is 0"
+                response["status"] = "error"
+            elif doc != predoc:
+                predoc = doc
+                f.write(doc)
+                # return "succes"
+                response["message"] = "success"
+                response["status"] = "success"
+
+            else:
+                # return "error:double reqest"
+                response["message"] = "error:double reqest"
+                response["status"] = "success"
+        # return "succes"
+
     except FileNotFoundError:
-        return "File not Found"
+        # return f"File not Found{req}"
+        response["message"] = f"File not Found{req}"
+        response["status"] = "error"
     except:
-        return "failed"
+        # return f"failed{req['snappath']}"
+        response["message"] = f"failed{req['snappath']}"
+        response["status"] = "error"
+    return jsonify(response)
 
-@app.route("/", methods=["post"])
-def postf():
-    # request.form["token"] #
-    response = jsonify({"token":"dummy"})
-    return response
-
-print("ss")
 app.run(host="127.0.0.1", port=8888, debug=True)
